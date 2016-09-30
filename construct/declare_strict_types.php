3ec5eb49c6040d43406cc18c8dc89d774dbbaad9
chenfanyue <nicegogogo@163.com>
2016-09-30 18:59:41 +0800

    Create declare_strict_types.php

---
+++ construct/declare_strict_types.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+// Without the statement above the default mode is coercive.
+
+function sum(int $a, int $b) :int {
+    return $a + $b;
+}
+
+var_dump(sum(1, 2));
+var_dump(sum(1.5, 2.5));
