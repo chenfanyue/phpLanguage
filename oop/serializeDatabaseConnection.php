b82be926675cc583236c0a0cc35ef90c058ecfc0
chenfanyue <nicegogogo@163.com>
2016-10-10 19:03:07 +0800

    Create serializeDatabaseConnection.php

---
+++ oop/serializeDatabaseConnection.php
@@ -0,0 +1,34 @@
+<?php
+class Connection
+{
+    protected $link;
+    private $dsn, $username, $password;
+    
+    public function __construct($dsn, $username, $password)
+    {
+        $this->dsn = $dsn;
+        $this->username = $username;
+        $this->password = $password;
+        $this->connect();
+    }
+    
+    private function connect()
+    {
+        $this->link = new PDO($this->dsn, $this->username, $this->password);
+    }
+    
+    public function __sleep()
+    {
+        return array('dsn', 'username', 'password');
+    }
+    
+    public function __wakeup()
+    {
+        $this->connect();
+    }
+}
+
+$c = new Connection('mysql:host=localhost;dbname=first', 'root', '');
+$s=serialize($c);
+var_dump($c);
+var_dump($s);
